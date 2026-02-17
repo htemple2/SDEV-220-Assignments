297aa1972763b389aa497557b9a73ba68eea7506
htemple2 <htemple2@ivytech.edu>
2026-02-17 08:11:55 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from my_sum import sum
+from fractions import Fraction
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+if __name__ == "__main_":
+    unittest.main()
+    
